5aaf35936cde263455ad56a5cd73b4dee4bcaa2b
Hystrix <2669731677@qq.com>
2024-07-10 21:43:12 +0800

    new

---
+++ node/frontend/finalPrincessPhone/js/i18n.js
@@ -0,0 +1,303 @@
+const translations = {
+  'zh-TW': {
+      "lang-prize": '獎勵',
+      "lang-group": '社群',
+      "lang-intro": '介紹',
+      "lang-attention": '注意事項',
+      "lang-beforeappoint": '事前預約',
+      "lang-appoint": '立即預約',
+      "lang-box1t1": '填寫手機號碼即可完成預約拿好禮',
+      "lang-pleasePhone": '請輸入手機號:',
+      "lang-HK": '香港',
+      "lang-TW": '台灣',
+      "lang-CO": '澳門',
+      "lang-KO": '韓國',
+      "lang-JP": '日本',
+      "lang-HK-phone": '香港 +852',
+      "lang-TW-phone": '台灣 +886',
+      "lang-CO-phone": '澳門 +853',
+      "lang-KO-phone": '韓國 +82',
+      "lang-JP-phone": '日本 +81',
+      "lang-inputPhone": '填寫手機號碼即可完成預約拿好禮',
+      "lang-agree": '同意隱私政策使用及接收獎勵資訊',
+      "lang-box1t2": '限時菁英封測',
+      "lang-box1text2": '限時菁英封測現已開放！現在就立即報名搶先體驗遊戲吧！<br>(封測只限 Android 用戶參與，名額有限，先到先得)',
+      "lang-attend": '立即加入',
+      "lang-followt": '關注官方社群抽最新Iphone 15promax',
+      "lang-followtext": '相關詳情請留意官方社交媒體',
+      "lang-official": '官方社群',
+      "lang-prize1": '銅幣X888',
+      "lang-condition": '預約人數達到',
+      "lang-num1":"100,000人",
+      "lang-num2":"200,000人",
+      "lang-num3":"300,000人",
+      "lang-num4":"500,000人",
+      "lang-prize2": '寶石X200',
+      "lang-prize3": '隨機紫卡X5',
+      "lang-prize4": '金色稀有度公主 － 雨蘭',
+      "lang-attentionT": '事前登錄網站',
+      "lang-attention1": '(1) 事前登錄活動時間為：即日起至遊戲正式上線前結束。',
+      "lang-attention2": '(2) 成功參與事前登錄的玩家，可在遊戲正式上線時收到禮包碼簡訊，遊戲上線後即可在遊戲內兌換獎勵。',
+      "lang-attention3": '(3) 實體獎勵得獎名單請關注官方粉絲團及社群之最新消息。',
+      "lang-attention4": '(4) 若有發現使用不當或非法手段參與活動，官方有權取消該玩家及所有相關帳號之得獎資格。',
+      "lang-attention5": '(5) 所有活動內容將以官方最後公告為主，保有變更、調整及終止本活動之權利。若變更內容將於官方網站或粉絲團公告，恕不另行通知。',
+      "lang-tip1": '本遊戲依遊戲軟體分級管理辦法分類為保護級6+。',
+      "lang-tip2": '本遊戲為免費使用，遊戲內另提供購買虛擬遊戲幣，物品等付費服務。',
+      "lang-privacy": '隱私條款',
+      "lang-service": '服務條款',
+      "princess_defend":"公主守不了",
+      "warrior":"PVP塔防遊戲",
+      "lang-success":"你已成功完成事前預約！<br>感謝您的參與，敬請期待《公主守不了》正式上市！"
+  },
+  en: {
+   "lang-prize": 'Reward',
+      "lang-group": 'Community',
+      "lang-intro": 'Features',
+      "lang-attention": 'Terms ',
+      "lang-beforeappoint": 'Pre-register',
+      "lang-appoint": 'Pre-register',
+      "lang-box1t1": 'Fill in your mobile phone number and get the pre-registration gift', 
+      "lang-pleasePhone": 'Please enter your mobile phone number:',
+      "lang-HK": 'HongKong',
+      "lang-TW": 'Taiwan',
+      "lang-CO": 'Macau',
+      "lang-KO": 'Korea',
+      "lang-JP": 'Japan',
+      "lang-HK-phone": 'HongKong+852',
+      "lang-TW-phone": 'Taiwan +886',
+      "lang-CO-phone": 'Macau +853',
+      "lang-KO-phone": 'Korea +82',
+      "lang-JP-phone": 'Japan +81',
+      "lang-inputPhone": 'Please enter your phone number',
+      "lang-agree": 'Agree to the Privacy Policy for the use and receipt of reward information',
+      "lang-box1t2": 'Limited Elite Beta Test',
+      "lang-box1text2": 'Elite Closed Beta is open! Sign up now! <br>(Android only, limited spots, first-come, first-served)',
+      "lang-attend": 'Join Now',
+      "lang-followt": 'Follow our community and grab a chance to win iPhone 15 Pro Max!',
+      "lang-followtext": 'Please stay tuned to our official social media for the raffle detail.',
+      "lang-official": 'Official Community',
+      "lang-condition": 'pre-registrations',
+      "lang-num1":"100,000",
+      "lang-num2":"200,000",
+      "lang-num3":"300,000",
+      "lang-num4":"500,000",
+      "lang-prize1": 'Coins x 888',
+      "lang-prize2": 'Gem X200',
+      "lang-prize3": 'Random Purple Card x 5',
+      "lang-prize4": 'Golden Rarity Princess - Yulan',
+      "lang-attentionT": 'Terms',
+      "lang-attention1": '(1) The pre-registration activity period is from now until the official game launch.',
+      "lang-attention2": '(2) Players who successfully participate in the pre-registration event can receive a gift code SMS when the game is officially launched, and can redeem rewards in the game after the game is launched.',
+      "lang-attention3": '(3) For the list of physical reward winners, please pay attention to the latest news of our official social media.',
+      "lang-attention4": '(4) If it is found that players participating in the event by improper or illegal means, the player and all related accounts will be disqualified',
+      "lang-attention5": '(5) All the contents of the event will be based on the official final announcement, and the right to change, adjust and terminate the event is reserved. Changes will be announced on the official website or fan group without prior notice.',
+      "lang-tip1": 'This game is classified as protection level 6+ according to the game software classification management measures.',
+      "lang-tip2": 'This game is free to play, and there are also paid services such as purchasing virtual game currency and items in the game.',
+      "lang-privacy": 'Privacy Policy',
+      "lang-service": 'Terms of Service',
+     "princess_defend":"Princess Can't Defend",
+      "warrior":"PVP Tower Defense Game",
+      "lang-success":"You have successfully completed your pre-registration!<br>Thank you for participating, and stay tuned for the official release of \"Princess Can't Defend!\""
+  },
+  'ko': {
+      "lang-prize": '선물',
+      "lang-group": '커뮤니티', 
+      "lang-intro": '게임 설명',
+      "lang-attention": '약관',
+      "lang-beforeappoint": '사전등록',
+      "lang-appoint": '예약하다',
+      "lang-box1t1": '휴대폰 번호를 입력하여 예약을 완료하고 선물을 받으세요.',
+      "lang-pleasePhone": '휴대 전화 번호를 입력하십시오:',
+      "lang-HK": '홍콩',
+      "lang-TW": '대만',
+      "lang-CO": '마카오',
+      "lang-KO": '한국',
+      "lang-JP": '일본',
+      "lang-HK-phone": '홍콩 +852',
+      "lang-TW-phone": '대만 +886',
+      "lang-CO-phone": '마카오 +853',
+      "lang-KO-phone": '한국 +82',
+      "lang-JP-phone": '일본 +81',
+      "lang-inputPhone": '휴대 전화 번호를 입력하십시오',
+      "lang-agree": '보너스 정보의 사용 및 수령에 대한 개인 정보 보호 정책에 동의합니다.',
+      "lang-box1t2": '한정 엘리트 베타 테스트',
+      "lang-box1text2": '기간 한정 엘리트 클로즈 베타가 시작되었습니다! 지금 얼리 액세스에 등록하세요!<br>(클로즈 베타는 안드로이드 사용자만 이용할 수 있으며, 장소 수는 제한되어 있으며 선착순으로 진행됩니다.)',
+      "lang-attend": '지금 가입하세요',
+      "lang-followt": '커뮤니티 팔로우하고 iPhone 15 Pro Max 당첨 기회 잡기!',
+      "lang-followtext": '추첨에 대한 자세한 내용은 공식 소셜 미디어를 계속 지켜봐 주시기 바랍니다.',
+      "lang-official": '공식 커뮤니티',
+      "lang-condition": '사전등록',
+      "lang-num1":"100,000명",
+      "lang-num2":"200,000명",
+      "lang-num3":"300,000명",
+      "lang-num4":"500,000명",
+      "lang-prize1": '코인 x 888',
+      "lang-prize2": '보석 x 200', 
+      "lang-prize3": '랜덤 퍼플카드 x 5',
+      "lang-prize4": '골든 레리티 프린세스 - Yulan',
+      "lang-attentionT": '약관',
+      "lang-attention1": '(1) 사전등록 활동 기간은 지금부터 정식 게임 출시일까지입니다.',
+      "lang-attention2": '(2) 사전등록 이벤트에 성공적으로 참여한 플레이어는 게임 정식 출시 시 선물코드 SMS를 받을 수 있으며, 게임 출시 후 게임 내에서 보상을 교환할 수 있습니다.',
+      "lang-attention3": '(3) 실물 리워드 당첨자 명단은 당사 공식 SNS의 최신 소식을 주목해 주시기 바랍니다.',
+      "lang-attention4": '(4) 플레이어가 부정하거나 부정한 방법으로 이벤트에 참가한 것이 적발된 경우 해당 플레이어 및 관련 계정 전체를 실격 처리합니다.',
+      "lang-attention5": '(5) 이벤트의 모든 내용은 공식 최종 공지를 기준으로 하며, 이벤트를 변경, 조정, 종료할 권리가 있습니다. 변경 사항은 사전 예고 없이 공식 홈페이지 또는 팬클럽을 통해 공지될 예정입니다.',
+      "lang-tip1": '이 게임은 게임 소프트웨어 분류 관리 조치에 따라 보호 수준 6+로 분류됩니다.',
+      "lang-tip2": '이 게임은 무료로 사용할 수 있으며 게임 내 가상 게임 화폐 및 아이템 구매와 같은 유료 서비스도 있습니다.',
+      "lang-privacy": '개인정보처리방침',
+      "lang-service": '서비스 약관',
+      "princess_defend":"공주는 지킬 수 없다",
+      "warrior":"PVP 타워 디펜스 게임",
+      "lang-success":"사전 예약을 성공적으로 완료하셨습니다!<br>참여해 주셔서 감사드리며, \"Princess Can't Defend\"의 정식 발매를 기대해 주세요!"
+  },
+  'ja': {
+"lang-prize": '特典',
+"lang-group": 'コミュニティ',
+"lang-intro": 'ゲーム紹介',
+"lang-attention": '条項',
+"lang-beforeappoint": '事前登録',
+"lang-appoint": '今すぐ予約',
+"lang-box1t1": '携帯電話番号を入力して事前登録プレゼントをゲットしよう',
+"lang-pleasePhone": '携帯電話番号を入力してください:',
+"lang-HK": '香港',
+"lang-TW": '台湾',
+"lang-CO": 'マカオ',
+"lang-KO": '韓国',
+"lang-JP": '日本',
+"lang-HK-phone": '香港 +852',
+      "lang-TW-phone": '台湾 +886',
+      "lang-CO-phone": 'マカオ +853',
+      "lang-KO-phone": '韓国 +82',
+      "lang-JP-phone": '日本 +81',
+"lang-inputPhone": '携帯番号を入力して登録特典をゲット',
+"lang-agree": '報酬情報の使用と受け取りに関するプライバシーポリシーに同意する',
+"lang-box1t2": '期間限定エリートCBテスト',
+"lang-box1text2": 'エリートクローズドベータが期間限定でオープン！今すぐ早期アクセスにサインアップを！<br>(Androidユーザー限定、先着順)',
+"lang-attend": '今すぐ登録',
+"lang-followt": '公式フォローでiPhone 15 Pro Maxを抽選でゲット!',
+"lang-followtext": '抽選の詳細については、公式ソーシャルメディアにご注目ください。',
+"lang-official": '公式コミュニティ',
+"lang-condition": '事前登録者数',
+"lang-num1":"100,000",
+"lang-num2":"200,000",
+"lang-num3":"300,000",
+"lang-num4":"500,000",
+"lang-prize1": 'コイン×888',
+"lang-prize2": 'ジェム×200',
+"lang-prize3": 'ランダムパープルカード×5',
+"lang-prize4": 'ゴールデンレアリティプリンセス - Yulan',
+"lang-attentionT": '条項',
+"lang-attention1": '(1)事前登録活動期間は、今から正式ゲーム発売までです。',
+"lang-attention2": '(2) 事前登録イベントに無事に参加したプレイヤーは、ゲームが正式にリリースされたときにギフトコードSMSを受け取ることができ、ゲームローンチ後にゲーム内で報酬を引き換えることができます。',
+"lang-attention3": '(3) 物理報酬の当選者リストは、公式SNSの最新ニュースにご注意ください。',
+"lang-attention4": '(4) プレイヤーが不正または違法な手段でイベントに参加していることが判明した場合、当該プレイヤーおよび関連するすべてのアカウントは失格となります',
+"lang-attention5": '(5) イベントの内容は全て公式最終発表に基づくものとし、イベントの変更・調整・終了の権利は留保されます。変更内容は予告なく公式サイトやファングループにて告知いたします。',
+"lang-tip1": 'このゲームは、ゲームソフトの分類管理措置により、保護レベル6+に分類されています。',
+"lang-tip2": 'このゲームは無料で遊べるほか、ゲーム内の仮想ゲーム通貨やアイテムの購入などの有料サービスもあります。',
+"lang-privacy": 'プライバシーポリシー',
+"lang-service": '利用規約',
+"princess_defend":"お姫様は守れない",
+      "warrior":"PVPタワーディフェンスゲーム",
+      "lang-success":"事前登録が完了しました!<br>ご参加ありがとうございました、そして \"Princess Can't Defend\" の正式リリースをお楽しみに!"
+},
+};
+
+let lang='zh-TW';
+  function switchLanguage(lang) {
+      // 更新页面内容
+      document.querySelector('.lang-prize').textContent = translations[lang]['lang-prize'];
+      document.querySelector('.lang-group').textContent = translations[lang]['lang-group'];
+      document.querySelector('.lang-intro').textContent = translations[lang]['lang-intro'];
+      document.querySelector('.lang-attention').textContent = translations[lang]['lang-attention'];
+      document.querySelector('.lang-beforeappoint').textContent = translations[lang]['lang-beforeappoint'];
+      document.querySelectorAll('.lang-appoint')[0].textContent = translations[lang]['lang-appoint'];
+      document.querySelectorAll('.lang-appoint')[1].textContent = translations[lang]['lang-appoint'];
+      document.querySelectorAll('.lang-appoint')[2].textContent = translations[lang]['lang-appoint'];
+      document.querySelector('.lang-box1t1').textContent = translations[lang]['lang-box1t1'];
+      document.querySelector('.lang-box1text2').innerHTML = translations[lang]['lang-box1text2'];
+      document.querySelector('.lang-pleasePhone').textContent = translations[lang]['lang-pleasePhone'];
+      document.querySelector('.lang-HK-phone:nth-child(1)').textContent = translations[lang]['lang-HK-phone'];
+      document.querySelector('.lang-HK-phone:nth-child(2)').textContent = translations[lang]['lang-HK-phone'];
+      document.querySelector('.lang-TW-phone').textContent = translations[lang]['lang-TW-phone'];
+      document.querySelector('.lang-CO-phone').textContent = translations[lang]['lang-CO-phone'];
+      document.querySelector('.lang-KO-phone').textContent = translations[lang]['lang-KO-phone'];
+      document.querySelector('.lang-JP-phone').textContent = translations[lang]['lang-JP-phone'];
+  
+      document.querySelector('.lang-inputPhone').placeholder  = translations[lang]['lang-inputPhone'];
+     const lang_agree= document.querySelector('.lang-agree');
+     lang_agree.textContent = translations[lang]['lang-agree'];
+      document.querySelector('.lang-box1t2').textContent = translations[lang]['lang-box1t2'];
+      document.querySelector('.lang-attend').textContent = translations[lang]['lang-attend'];
+      document.querySelector('.lang-followt').textContent = translations[lang]['lang-followt'];
+      document.querySelector('.lang-followtext').textContent = translations[lang]['lang-followtext'];
+      document.querySelectorAll('.lang-official').forEach(function(item){
+        item.textContent= translations[lang]['lang-official'];
+      })
+      document.querySelector('.lang-prize1').textContent = translations[lang]['lang-prize1'];
+      document.querySelectorAll('.lang-condition').forEach(function(item){
+        item.textContent= translations[lang]['lang-condition'];
+      })
+      document.querySelector('.lang-num1').textContent = translations[lang]['lang-num1'];
+      document.querySelector('.lang-num2').textContent = translations[lang]['lang-num2'];
+      document.querySelector('.lang-num3').textContent = translations[lang]['lang-num3'];
+      document.querySelector('.lang-num4').textContent = translations[lang]['lang-num4'];
+      document.querySelector('.lang-prize2').textContent = translations[lang]['lang-prize2'];
+      
+
+      document.querySelector('.lang-prize3').textContent = translations[lang]['lang-prize3'];
+    
+      document.querySelector('.lang-prize4').textContent = translations[lang]['lang-prize4'];
+     
+      document.querySelector('.lang-attentionT').textContent = translations[lang]['lang-attentionT'];
+      document.querySelector('.lang-attention1').textContent = translations[lang]['lang-attention1'];
+      document.querySelector('.lang-attention2').textContent = translations[lang]['lang-attention2'];
+      document.querySelector('.lang-attention3').textContent = translations[lang]['lang-attention3'];
+      document.querySelector('.lang-attention4').textContent = translations[lang]['lang-attention4'];
+      document.querySelector('.lang-attention5').textContent = translations[lang]['lang-attention5'];
+      document.querySelector('.lang-tip1').textContent = translations[lang]['lang-tip1'];
+      document.querySelector('.lang-tip2').textContent = translations[lang]['lang-tip2'];
+     const privacy= document.querySelector('.lang-privacy');
+     privacy.textContent = translations[lang]['lang-privacy'];
+     const service= document.querySelector('.lang-service');
+     service.textContent = translations[lang]['lang-service'];
+     document.querySelector('.princess_defend').textContent = translations[lang]['princess_defend'];
+     document.querySelector('.warrior').textContent = translations[lang]['warrior'];
+     document.querySelector('.lang-success').innerHTML = translations[lang]['lang-success'];
+     const logo = document.querySelector('.logo');
+     const gameImg1=document.querySelector('.gameImg1');
+     const gameImg2=document.querySelector('.gameImg2');
+     const gameImg3=document.querySelector('.gameImg3');
+     const gameImg4=document.querySelector('.gameImg4');
+     const gameImg5=document.querySelector('.gameImg5');
+     if(lang==='zh-TW'){
+      lang_agree.href="Tnc.html"
+      privacy.href="Privacy.html"
+      service.href="Tnc.html"
+   logo.style.backgroundImage = 'url(/finalPrincessPhone/images/header/background/Logo.png)';
+   gameImg1.src='/finalPrincessPhone/images/main/box4/intro1.png';
+   gameImg2.src='/finalPrincessPhone/images/main/box4/intro2.png';
+   gameImg3.src='/finalPrincessPhone/images/main/box4/intro3.png';
+   gameImg4.src='/finalPrincessPhone/images/main/box4/intro4.png';
+   gameImg5.src='/finalPrincessPhone/images/main/box4/intro5.png';
+    }else{
+      lang_agree.href="TncEN.html"
+      privacy.href="PrivacyEN.html"
+      service.href="TncEN.html"
+      logo.style.backgroundImage = 'url(/finalPrincessPhone/images/header/background/LogoEN.png)';
+      gameImg1.src='/finalPrincessPhone/images/main/box4/intro1EN.png';
+      gameImg2.src='/finalPrincessPhone/images/main/box4/intro2EN.png';
+      gameImg3.src='/finalPrincessPhone/images/main/box4/intro3EN.png';
+      gameImg4.src='/finalPrincessPhone/images/main/box4/intro4EN.png';
+      gameImg5.src='/finalPrincessPhone/images/main/box4/intro5EN.png';
+    }
+    }
+
+const langSwitcher = document.querySelectorAll('.language');
+
+langSwitcher.forEach(btn => {
+btn.addEventListener('click', () => {
+  lang = btn.dataset.lang;
+  switchLanguage(lang);
+  console.log(btn.dataset.lang);
+});
+});
+// 默认设置为繁体中文
---
+++ node/frontend/finalPrincessPhone/js/index.js
@@ -0,0 +1,317 @@
+
+
+let phoneInput = document.getElementById('phoneText');
+let AgreeCheck = document.getElementById('agreeCheck');
+const selectRegions = document.querySelectorAll('.selectRegion');
+const commitPhoneElement = document.getElementById('commit');
+const phoneRegex = {
+  TW: /^[0-9]{9}$/,
+  HK: /^[0-9]{8}$/,
+  MO: /^[0-9]{8}$/,
+  JP: /^[0-9]{10}$/,
+  KR: /^[0-9]{10,11}$/
+};
+// 手机号输入监听器
+phoneInput.addEventListener('input', (event) => {
+  if (event.target.value.length > 11) {
+    event.target.value = event.target.value.slice(0, 11);
+  }
+  event.target.value = event.target.value.replace(/\D/g, '');
+  console.log(event.target.value);
+});
+
+
+//
+
+function alertError(category,lang){
+  if(category===0){
+    if(lang==='zh-TW'){
+      alert('手機號爲空!');
+    }else if(lang==='en'){
+      alert('The mobile phone number cannot be empty!');
+    }else if(lang==='ko'){
+      alert('휴대폰 번호는 비워 둘 수 없습니다！');
+    }else if(lang==='ja'){
+      alert('手机番号が空です');
+    }
+  }else if(category===1){
+    if(lang==='zh-TW'){
+      alert('手機號格式錯誤!');
+    }else if(lang==='en'){
+      alert('The phone number is in the wrong format!');
+    }else if(lang==='ko'){
+      alert('전화 번호의 형식이 잘못되었습니다！');
+    }else if(lang==='ja'){
+      alert('電話番号の形式が間違っている!');
+    }
+  }
+  else if(category===2){
+    if(lang==='zh-TW'){
+      alert('請同意服務條款!');
+    }else if(lang==='en'){
+      alert('Please agree to the Terms of Service!');
+    }else if(lang==='ko'){
+      alert('서비스 약관에 동의하십시오！');
+    }else if(lang==='ja'){
+      alert('利用規約に同意してください!');
+    }
+  }
+  else if(category===3){
+    if(lang==='zh-TW'){
+      alert('請選擇地區！');
+    }else if(lang==='en'){
+      alert('Please select a region！');
+    }else if(lang==='ko'){
+      alert('지역을 선택해 주세요！');
+    }else if(lang==='ja'){
+      alert('地域を選択してください！');
+    }
+  }
+  else if(category===4){
+    if(lang==='zh-TW'){
+      alert('本手機號碼已完成預約!');
+    }else if(lang==='en'){
+      alert('This mobile phone number has completed the reservation！');
+    }else if(lang==='ko'){
+      alert('이 휴대폰 번호로 예약이 완료되었습니다！');
+    }else if(lang==='ja'){
+      alert('この携帯電話番号は予約を完了しました!');
+    }
+  }
+}
+// 提交按钮点击事件监听器
+commitPhoneElement.addEventListener('click', () => {
+  // 检查复选框是否被勾选
+console.log(lang)
+  // 检查手机号格式
+  let phoneValue = phoneInput.value;
+  if (phoneValue==='') {
+     alertError(0,lang);
+    return;
+  }  
+
+  if (isNaN(phoneValue)) {
+    alertError(1,lang)
+    return;
+  }
+
+  // 检查下拉框选择情况
+  let selectedRegion = selectRegions[0].getAttribute('value');
+  if (selectedRegion === 'none') {
+    alertError(3,lang);
+    return;
+  }
+
+  if(selectedRegion==="886"){
+    if(!phoneRegex.TW.test(phoneValue)){
+      alertError(1,lang)
+      return
+    }
+  }
+  if(selectedRegion==="852"){
+    if(!phoneRegex.HK.test(phoneValue)){
+      alertError(1,lang)
+      return
+    }
+  }
+  if(selectedRegion==="853"){
+    if(!phoneRegex.MO.test(phoneValue)){
+      alertError(1,lang)
+      return
+    }
+  }
+  if(selectedRegion==="81"){
+    if(!phoneRegex.JP.test(phoneValue)){
+      alertError(1,lang)
+      return
+    }
+  }
+  if(selectedRegion==="82"){
+    if(!phoneRegex.KR.test(phoneValue)){
+      alertError(1,lang)
+      return
+    }
+  }
+
+  let isAgreeCheck = AgreeCheck.checked;
+  console.log('isAgreeCheck:', isAgreeCheck);
+   if(!isAgreeCheck){
+    alertError(2,lang)
+    return;
+   }
+  // 所有检查通过后,执行提交操作
+
+  // 在这里添加您的提交逻辑
+  fetch('http://18.142.183.247:8084/api/commitPhone', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    'Accept': 'application/json'
+    },
+    body: JSON.stringify({ region: selectedRegion, phone: phoneValue })
+  })
+  .then(response => response.json())
+  .then(data => {
+    if(data.error==="手机号和地区已存在"){
+      alertError(4,lang);
+      return;
+    }
+    // 在这里添加其他成功后的逻辑
+    // 获取弹窗元素
+    let modal = document.querySelector('.modal-container');
+     modal.style.display="block"
+    // 获取关闭按钮元素
+    let closeButton = document.querySelector('.close-button');
+    // 显示弹窗
+    //let menuCloseButton = document.querySelector('.close-button');
+
+    closeButton.addEventListener('click', function() {
+      modal.style.display = 'none';
+    });
+    
+    // 点击弹窗外部区域也可以隐藏弹窗
+    window.addEventListener('click', function(event) {
+      if (event.target == modal) {
+        modal.style.display = 'none';
+      }
+    });
+
+
+
+  })
+  .catch(error => {
+    // 在这里添加错误处理的逻辑
+  });
+});
+
+
+const beforeappoint = document.querySelector('.button')
+const appoint = document.querySelector('.appoint')
+const target =document.getElementById('appoint')
+beforeappoint.addEventListener('click', () => {
+    target.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+ appoint.addEventListener('click', () => {
+    target.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+const attend=document.querySelector('.attend')
+attend.addEventListener('click', () => {
+  window.open('https://www.facebook.com/princesscantdefend/posts/pfbid0kVantVTYHhNi9dTeq3GfxuAvx2CQWMHW6fmvRdTBQCVwvSAGp4Uc3XPEQr43oNWCl', '_blank');
+}
+)
+
+var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 3,
+      spaceBetween: 30,
+	  centeredSlides: true,
+	  loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+    
+    var swiper1 = new Swiper(".mySwiper1", {
+      effect: "cards",
+      grabCursor: true,
+    });
+  
+    var swiper = new Swiper(".mySwiper2", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
+
+ 
+// 获取所有菜单项的链接
+const menuLinks = document.querySelectorAll('.menu .guideBox a');
+const closeButton = document.querySelector('.menuclose');
+const menu = document.querySelector('.menu');
+
+// 为每个菜单项添加点击事件监听器
+menuLinks.forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault(); // 阻止默认链接跳转行为
+    closeMenu(); // 关闭菜单弹窗
+    scrollToSection(e.target.getAttribute('href')); // 滚动到相应位置
+  });
+});
+const menuButton=  document.querySelector('.menuButton');
+  menuButton.addEventListener('click', () => {
+    menu.style.display='block';
+  });
+// 关闭菜单弹窗的函数
+function closeMenu() {
+  menu.style.display = 'none'; // 隐藏菜单弹窗
+}
+
+// 滚动到相应位置的函数
+function scrollToSection(sectionId) {
+  let targetElement;
+  switch (sectionId) {
+    case 'menuAppoint':
+      targetElement = document.querySelector('#appoint');
+      break;
+    case 'menuIntro':
+      targetElement = document.querySelector('#intro');
+      break;
+    case 'menuattention':
+      targetElement = document.querySelector('#attention');
+      break;
+  }
+  if (targetElement) {
+    targetElement.scrollIntoView({ behavior: 'smooth' }); // 平滑滚动到目标位置
+  }
+}
+
+// 为关闭按钮添加点击事件监听器
+closeButton.addEventListener('click', closeMenu);
+
+
+
+// 获取所有菜单项的链接
+const languagePoplinks = document.querySelectorAll('.languagePop .guideBox a');
+const backButton = document.querySelector('.menuback');
+const languagePop = document.querySelector('.languagePop');
+// 为每个菜单项添加点击事件监听器
+languagePoplinks.forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault(); // 阻止默认链接跳转行为
+    backMenu(); // 关闭菜单弹窗
+    closeMenu();
+    switchLanguage(link.dataset.lang);
+  });
+});
+const languageButton=  document.querySelector('.languageButton');
+languageButton.addEventListener('click', () => {
+    languagePop.style.display='block';
+    menu.style.display='none';
+  });
+// 关闭菜单弹窗的函数
+function backMenu() {
+    languagePop.style.display = 'none'; // 隐藏菜单弹窗
+    menu.style.display='block';
+}
+
+// 滚动到相应位置的函数
+
+
+// 为关闭按钮添加点击事件监听器
+backButton.addEventListener('click', backMenu);
